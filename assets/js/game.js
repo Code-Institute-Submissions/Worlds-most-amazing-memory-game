5b7bf089c50337ac933a3c2b427eb0e6c9416c92
fergusthompson <fergus.thompson@hotmail.com>
2020-06-18 11:19:21 +0000

    added data framework to cards

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,7 +1,23 @@
 const memoryCards = document.querySelectorAll('.memory-card');
 
+let cardFlipped = false;
+let firstCard, secondCard;
+
 function flipCard(){
-    this.classList.toggle('flip');
+    this.classList.add('flip');
+
+    if(cardFlipped) {
+    // first click
+    cardFlipped = true;
+    firstCard = this;
+    // second click
+  } else {
+      cardFlipped = false;
+      secondCard = this;
+
+      //check to see if cards match?
+      
+  }
 }
 
 memoryCards.forEach(card => card.addEventListener('click', flipCard));
