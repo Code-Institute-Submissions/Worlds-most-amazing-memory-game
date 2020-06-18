e695e10be420d5172c347fd84cdb8540899a3bcc
fergusthompson <fergus.thompson@hotmail.com>
2020-06-18 10:31:53 +0000

    added flip to cards

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,7 +1,7 @@
 const memoryCards = document.querySelectorAll('.memory-card');
 
 function flipCard(){
-    
+    this.classList.toggle('flip');
 }
 
 memoryCards.forEach(card => card.addEventListener('click', flipCard));
