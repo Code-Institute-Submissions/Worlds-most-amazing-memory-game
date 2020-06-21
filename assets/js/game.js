5178064d46757c508dc3c1cf80ad1f47ba2db9d0
fergusthompson <fergus.thompson@hotmail.com>
2020-06-21 09:13:50 +0000

    added comments to js

--- assets/js/game.js
+++ assets/js/game.js
@@ -5,6 +5,9 @@ let memoryCard1, memoryCard2;
 let stopUser = false;
 
 function flipCard(){
+
+//flips the cards over
+
     if (stopUser) return;
     if (this === memoryCard1) return;
     this.classList.add('flip');
@@ -28,9 +31,11 @@ function checkCardsMatch() {
        let cardsMatch = memoryCard1.dataset.framework === memoryCard2.dataset.framework;
 
        cardsMatch ? disableCardFlip() : removeFlipClass();
+       
       
     }
 
+// stops user clicking flipped card 
 
 function disableCardFlip(){
        memoryCard1.removeEventListener('click', flipCard);
@@ -55,7 +60,7 @@ function resetGame(){
     [cardFlipped, stopUser] = [false, false];
     [memoryCard1, memoryCard2] = [null,null];
 }
-
+// shufles teh cards at the beginning of the game
 (function shuffleHeroes(){
     memoryCards.forEach(card => {
         let randomize = Math.floor(Math.random() * 16);
@@ -63,5 +68,12 @@ function resetGame(){
 
     });
 })();
+
+function youWin(){
+    if (memoryCards === ('flip')) return;
+     window.alert('you Win!!');
+
+    
+}
 
 memoryCards.forEach(card => card.addEventListener('click', flipCard));
