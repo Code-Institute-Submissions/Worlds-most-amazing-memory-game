0287200fef04cb78309ceb306f34f9af873acb31
fergusthompson <fergus.thompson@hotmail.com>
2020-07-02 09:19:37 +0000

    cleaned up css and beautified

--- assets/js/game.js
+++ assets/js/game.js
@@ -2,72 +2,67 @@ const gameRef = document.querySelector('#card-row');
 const clickcountRef = document.querySelector('#click-counter');
 const outcomeScreenRef = document.querySelector('#memory-game');
 
-
 let cardFlipped = false;
 let firstClickedMemoryCard;
 let secondClickedMemoryCard;
 let stopUser = false;
-let winCondition = 0;
+let winCondition = 7;
 let loseCondition = 40;
 
-
-
-
-const heroes = [ 
-  {
+const heroes = [{
     name: 'black-widow',
   },
   {
     name: 'captain-america',
   },
-   {
+  {
     name: 'captain-marvel',
   },
   {
     name: 'hawkeye',
   },
-   {
+  {
     name: 'iron-man',
   },
   {
     name: 'spiderman',
   },
-   {
+  {
     name: 'thanos',
   },
   {
     name: 'the-hulk',
   }
-  
+
 ];
 
 const duplicateHeroes = heroes.concat(heroes);
 
 
 /**
- * 
- * @param {} shufflingHeroes shuffles the heroes before theyre written to html
+ * shufflingHeroes shuffles the heroes before theyre written to html
+ * @param {} 
  */
 const shuffle = (shufflingHeroes) => {
   shufflingHeroes.sort(() => Math.random() - 0.5);
 };
 
 /**
- * 
- * @param {} heroCards takes the shuffled cards and writes them to html
+ * heroCards takes the shuffled cards and writes them to html
+ * @param {} 
  */
-  const createHtmlForGame = (heroCards) => {
-    heroCards.forEach((hero) => {
+const createHtmlForGame = (heroCards) => {
+  heroCards.forEach((hero) => {
     // Create HTML
 
 
-        gameRef.innerHTML +=  ` 
+    gameRef.innerHTML += ` 
         <div class="memory-card col-4 col-md-3 col-lg-3 my-1" data-framework="${hero.name}">
          <img class="back-face" src="assets/images/marvel-logo.jpg" alt="Marvel Logo">
           <img class="front-face" src="assets/images/${hero.name}.jpg" alt="${hero.name}">
         </div>`;
   });
-  
+
 };
 // Shuffle the heroes
 shuffle(duplicateHeroes);
@@ -75,115 +70,116 @@ shuffle(duplicateHeroes);
 // Write HTML with shuffled heroes
 createHtmlForGame(duplicateHeroes);
 
-/**
- * adss the click counter when the game loads
- */
-  clickcountRef.innerHTML ="<div class='lose-counter'>40 Clicks Left!</div>";
+clickcountRef.innerHTML = "<div class='lose-counter'>40 Clicks Left!</div>";
 
- /**
+/**
  * adds the flip class and then runs check for match
  */
- const memoryCards = document.querySelectorAll('.memory-card');
+const memoryCards = document.querySelectorAll('.memory-card');
+memoryCards.forEach(card => card.addEventListener('click', flipCard));
 
- function flipCard(){
-    loseCondition -= 1;
-    checkCondition();
-    clickcountRef.innerHTML ='<div class="lose-counter">'+loseCondition+'  Clicks Left!</div>';
-    
-    if (stopUser) return; 
-    if (this === firstClickedMemoryCard) return; 
-    this.classList.add('flip');
-    if(!cardFlipped) {
+function flipCard() {
+  loseCondition -= 1;
+  checkCondition();
+  clickcountRef.innerHTML = '<div class="lose-counter">' + loseCondition +
+    '  Clicks Left!</div>';
+
+  if (stopUser) return;
+  if (this === firstClickedMemoryCard) return;
+  this.classList.add('flip');
+  if (!cardFlipped) {
     cardFlipped = true;
     firstClickedMemoryCard = this;
-        return;
-    }
-      secondClickedMemoryCard = this;
-    checkCardsMatch();  
-    
+    return;
   }
+  secondClickedMemoryCard = this;
+  checkCardsMatch();
+
+}
 
 /**
  * checks that cards match
  */
 const checkCardsMatch = () => {
 
-       let cardsMatch = firstClickedMemoryCard.dataset.framework === secondClickedMemoryCard.dataset.framework;
+  let cardsMatch = firstClickedMemoryCard.dataset.framework ===
+    secondClickedMemoryCard.dataset.framework;
 
-       cardsMatch ? disableCardFlip() : removeFlipClass();
+  cardsMatch ? disableCardFlip() : removeFlipClass();
 
-    }
-    /**
-     * checks to make sure you dont lose if you get the final pair on your final click
-     */
-    const checkCondition = () =>{
-        if (winCondition === 8){
-        showOutcomeScreen(true);
-        return;
-      }
-       if (loseCondition === 0 && winCondition != 8)
-       showOutcomeScreen(false);
-       return;
-    }
-    
+}
+/**
+ * checks to make sure you dont lose if you get the final pair on your final click
+ */
+const checkCondition = () => {
+  if (winCondition === 8) {
+    showOutcomeScreen(true);
+    return;
+  }
+  if (loseCondition === 0 && winCondition != 8)
+    showOutcomeScreen(false);
+  return;
+}
 
 
-    /** 
+
+/** 
  * stops user interacting with matched cards card 
- * */ 
+ * */
 const disableCardFlip = () => {
-       firstClickedMemoryCard.removeEventListener('click', flipCard);
-       secondClickedMemoryCard.removeEventListener('click', flipCard);
-       winCondition +=1;
-    checkCondition();
-    
-       resetGame();
+  firstClickedMemoryCard.removeEventListener('click', flipCard);
+  secondClickedMemoryCard.removeEventListener('click', flipCard);
+  winCondition += 1;
+  checkCondition();
+
+  resetGame();
 }
 
 /**
  * if the cards dont match they turn back over
  */
-const removeFlipClass = () =>{
+const removeFlipClass = () => {
 
-    stopUser = true;
-          setTimeout(() =>{
-          firstClickedMemoryCard.classList.remove('flip');
-          secondClickedMemoryCard.classList.remove('flip');
+  stopUser = true;
+  setTimeout(() => {
+    firstClickedMemoryCard.classList.remove('flip');
+    secondClickedMemoryCard.classList.remove('flip');
 
-          resetGame();
+    resetGame();
 
-      }, 1500);
+  }, 1500);
 }
 /** 
  * stops a bug when the user tries to click the same first card twice
  */
-const resetGame = () =>{
-    [cardFlipped, stopUser] = [false, false];
-    [firstClickedMemoryCard, secondClickedMemoryCard] = [null,null];
+const resetGame = () => {
+  [cardFlipped, stopUser] = [false, false];
+  [firstClickedMemoryCard, secondClickedMemoryCard] = [null, null];
 }
 
-  
+
 /**
  * resets the game 
  */
 const playAgain = () => {
-      location.reload();
+  location.reload();
 }
 
 /**
- * @param {*} winorlose shows the win or lose screen 
+ * winorlose shows the win or lose screen 
+ * @param {} 
  */
 const showOutcomeScreen = (winorlose) => {
-let outcomeText;
-winorlose ? (outcomeText = `You Win!`) : (outcomeText = `You Lose!`)
+  let outcomeText;
+  winorlose ? (outcomeText = `You Win!`) : (outcomeText = `You Lose!`)
 
 
-outcomeScreenRef.innerHTML=`
+  outcomeScreenRef.innerHTML = `
     <div class="outcomeScreen">
      <h2 class="outcometext">${outcomeText}</h2>
        <button id="play-again">Play Again</button>
      </div>`;
-    document.getElementById('play-again').addEventListener("click", playAgain);
+  document.getElementById('play-again').addEventListener("click", playAgain);
 }
 
 
